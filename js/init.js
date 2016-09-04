9b41fe584dbcad8787c61c3ff85189baf6520c44
Affan Ahmad Fahmi <affanahmadfahmi@gmail.com>
2016-09-04 18:11:44 +0530

    profile

--- js/init.js
+++ js/init.js
@@ -5,3 +5,92 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(function () {
+  "use strict";
+
+  $("#affan").click(function () {
+    $(".affan").fadeIn();
+    $("#download-button").hide();
+  });
+
+  $("span").click(function () {
+    $(".affan").fadeOut()
+    $("#download-button").show();
+  });
+
+});
+
+$(function () {
+  "use strict";
+
+  $("#syan").click(function () {
+    $(".syan").fadeIn();
+    $("#download-button").hide();
+  });
+
+  $("span").click(function () {
+    $(".syan").fadeOut()
+    $("#download-button").show();
+  });
+
+});
+
+$(function () {
+  "use strict";
+
+  $("#podar").click(function () {
+    $(".podar").fadeIn();
+    $("#download-button").hide();
+  });
+
+  $("span").click(function () {
+    $(".podar").fadeOut()
+    $("#download-button").show();
+  });
+
+});
+
+$(function () {
+  "use strict";
+
+  $("#chy").click(function () {
+    $(".chy").fadeIn();
+    $("#download-button").hide();
+  });
+
+  $("span").click(function () {
+    $(".chy").fadeOut()
+    $("#download-button").show();
+  });
+
+});
+$(function () {
+  "use strict";
+
+  $("#nit").click(function () {
+    $(".nit").fadeIn();
+    $("#download-button").hide();
+  });
+
+  $("span").click(function () {
+    $(".nit").fadeOut()
+    $("#download-button").show();
+  });
+
+});
+
+$(function () {
+  "use strict";
+
+  $("#unknown").click(function () {
+    $(".unknown").fadeIn();
+    $("#download-button").hide();
+  });
+
+  $("span").click(function () {
+    $(".unknown").fadeOut()
+    $("#download-button").show();
+  });
+
+});
